e9f3cccb695a935079885fd7b6f47e6967298f59
pikokr <parangee9706@gmail.com>
2021-01-06 19:33:10 +0900

    feat: apply API on user dropdown in header

---
+++ components/Layout/Header.tsx
@@ -0,0 +1,80 @@
+import { gql } from 'apollo-boost'
+import Link from 'next/link'
+import React from 'react'
+import { Query } from 'react-apollo'
+import Dropdown from '../Dropdown'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import clsx from 'clsx'
+
+const Header = () => {
+  return (
+    <div
+      className="mx-auto py-2 px-2 md:px-32 dark:bg-discord-black dark:text-white"
+      style={{ alignItems: 'center' }}
+    >
+      <div className="flex justify-between">
+        <div
+          className="text-2xl flex-grow flex"
+          style={{ alignItems: 'center' }}
+        >
+          <Link href="/">UNIQUEBOTS</Link>
+        </div>
+        <div style={{ alignItems: 'center' }} className="flex">
+          <Query
+            query={gql`
+              query {
+                user: me {
+                  id
+                  tag
+                  avatarURL
+                }
+                loginURL
+              }
+            `}
+          >
+            {({ loading, data }: any) => {
+              if (loading) return <div>Loading...</div>
+              if (data) {
+                const user = data.user
+                return data.user ? (
+                  <Dropdown
+                    leftOffset={-20}
+                    button={({ opened }) => (
+                      <div
+                        className="inline-flex select-none cursor-pointer"
+                        style={{ alignItems: 'center' }}
+                      >
+                        <img
+                          src={user.avatarURL}
+                          className="h-6 w-6 rounded-full mr-2"
+                          alt="Avatar"
+                        />
+                        <span className="md:block hidden">{user.tag}</span>
+                        <FontAwesomeIcon
+                          icon={['fas', 'angle-down']}
+                          size="2x"
+                          className={clsx('ml-2 transition-transform', {
+                            'fa-rotate-180': opened,
+                          })}
+                        />
+                      </div>
+                    )}
+                  >
+                    <div className="flex flex-column">
+                      <div>테스트입니다</div>
+                    </div>
+                  </Dropdown>
+                ) : (
+                  <a href={data.loginURL}>로그인</a>
+                )
+              }
+              return null
+            }}
+          </Query>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,70 +1,11 @@
 import React, { Component } from 'react'
-import Link from 'next/link'
-// import Dropdown from '../Dropdown'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import clsx from 'clsx'
-import { Query } from 'react-apollo'
-import { gql } from 'apollo-boost'
+import Header from './Header'
 
 class Layout extends Component {
   render() {
     return (
       <div>
-        <div
-          className="mx-auto py-2 px-2 lg:px-64 dark:bg-discord-black dark:text-white"
-          style={{ alignItems: 'center' }}
-        >
-          <div className="flex justify-between">
-            <div className="text-2xl flex-grow">
-              <Link href="/">UNIQUEBOTS</Link>
-            </div>
-            <div style={{ alignItems: 'center' }} className="flex">
-              {/*<Dropdown
-                leftOffset={-20}
-                button={({ opened }) => (
-                  <div
-                    className="inline-flex select-none cursor-pointer"
-                    style={{ alignItems: 'center' }}
-                  >
-                    <span>드롭다운</span>
-                    <FontAwesomeIcon
-                      icon={['fas', 'angle-down']}
-                      size="2x"
-                      className={clsx('ml-2 transition-transform', {
-                        'fa-rotate-180': opened,
-                      })}
-                    />
-                  </div>
-                )}
-              >
-                <div className="flex flex-column">
-                  <div>테스트입니다</div>
-                </div>
-              </Dropdown>*/}
-              <Query
-                query={gql`
-                  query {
-                    user: me {
-                      id
-                    }
-                    loginURL
-                  }
-                `}
-              >
-                {({ loading, data }: any) => {
-                  if (loading) return <div>Loading...</div>
-                  if (data)
-                    return data.user ? (
-                      <div></div>
-                    ) : (
-                      <a href={data.loginURL}>로그인</a>
-                    )
-                  return null
-                }}
-              </Query>
-            </div>
-          </div>
-        </div>
+        <Header />
         <div className="md:px-24 px-2">{this.props.children}</div>
       </div>
     )
