9bd8657e8c6914e5d060ba12a033566d053fa197
pikokr <parangee9706@gmail.com>
2021-01-06 10:43:48 +0900

    feat: layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Layout from '../components/Layout';
 import '../styles/global.css'
 
 const MyApp = ({Component, pageProps}: any) => {
-    return <Component {...pageProps}/>;
+    return  <Layout>
+        <Component {...pageProps}/>
+    </Layout>
 };
 
 export default MyApp;
